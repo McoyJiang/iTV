13d345dde6089cf0ba9ccce3f778480bc0689b0c
Danny.Jiang <danny.jiang@ef.com>
2019-11-25 23:07:41 +0800

    remove unecessary code

--- ai_tv/lib/widgets/Demo.dart
+++ ai_tv/lib/widgets/Demo.dart
@@ -34,64 +34,6 @@ class _DemoState extends State<Demo> {
 }
 
 class _MyHomePageState extends State<Demo> {
-  final _documents = [
-    {
-      'name': 'Learning Android Studio',
-      'link':
-      'http://barbra-coco.dyndns.org/student/learning_android_studio.pdf'
-    },
-    {
-      'name': 'Android Programming Cookbook',
-      'link':
-      'http://enos.itcollege.ee/~jpoial/allalaadimised/reading/Android-Programming-Cookbook.pdf'
-    },
-    {
-      'name': 'iOS Programming Guide',
-      'link':
-      'http://darwinlogic.com/uploads/education/iOS_Programming_Guide.pdf'
-    },
-    {
-      'name': 'Objective-C Programming (Pre-Course Workbook',
-      'link':
-      'https://www.bignerdranch.com/documents/objective-c-prereading-assignment.pdf'
-    },
-  ];
-
-  final _images = [
-    {
-      'name': 'Arches National Park',
-      'link':
-      'https://upload.wikimedia.org/wikipedia/commons/6/60/The_Organ_at_Arches_National_Park_Utah_Corrected.jpg'
-    },
-    {
-      'name': 'Canyonlands National Park',
-      'link':
-      'https://upload.wikimedia.org/wikipedia/commons/7/78/Canyonlands_National_Park%E2%80%A6Needles_area_%286294480744%29.jpg'
-    },
-    {
-      'name': 'Death Valley National Park',
-      'link':
-      'https://upload.wikimedia.org/wikipedia/commons/b/b2/Sand_Dunes_in_Death_Valley_National_Park.jpg'
-    },
-    {
-      'name': 'Gates of the Arctic National Park and Preserve',
-      'link':
-      'https://upload.wikimedia.org/wikipedia/commons/e/e4/GatesofArctic.jpg'
-    }
-  ];
-
-  final _videos = [
-    {
-      'name': 'Big Buck Bunny',
-      'link':
-      'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
-    },
-    {
-      'name': 'Elephant Dream',
-      'link':
-      'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'
-    }
-  ];
 
   List<_TaskInfo> _tasks;
   List<_ItemHolder> _items;
@@ -103,8 +45,6 @@ class _MyHomePageState extends State<Demo> {
   @override
   void initState() {
     super.initState();
-
-    FlutterDownloader.initialize();
 
     _bindBackgroundIsolate();
 
@@ -446,28 +386,8 @@ class _MyHomePageState extends State<Demo> {
     _tasks = [];
     _items = [];
 
-    _tasks.addAll(_documents.map((document) =>
-        _TaskInfo(name: document['name'], link: document['link'])));
+    _tasks.addAll(widget.response.seriesList.map((item) => _TaskInfo(name: item.zhuti, link: item.downloadUrl)));
 
-    _items.add(_ItemHolder(name: 'Documents'));
-    for (int i = count; i < _tasks.length; i++) {
-      _items.add(_ItemHolder(name: _tasks[i].name, task: _tasks[i]));
-      count++;
-    }
-
-    _tasks.addAll(_images
-        .map((image) => _TaskInfo(name: image['name'], link: image['link'])));
-
-    _items.add(_ItemHolder(name: 'Images'));
-    for (int i = count; i < _tasks.length; i++) {
-      _items.add(_ItemHolder(name: _tasks[i].name, task: _tasks[i]));
-      count++;
-    }
-
-    _tasks.addAll(_videos
-        .map((video) => _TaskInfo(name: video['name'], link: video['link'])));
-
-    _items.add(_ItemHolder(name: 'Videos'));
     for (int i = count; i < _tasks.length; i++) {
       _items.add(_ItemHolder(name: _tasks[i].name, task: _tasks[i]));
       count++;
